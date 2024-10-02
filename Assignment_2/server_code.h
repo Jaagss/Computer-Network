f32a99816b7d3631d1ff7f010de3009ed65f8b90
Aditya Jagadale <jagadaleaditya057@gmail.com>
2024-10-02 09:59:47 +0530

    commit: Final submission

--- Assignment_2/server_code.h
+++ Assignment_2/server_code.h
@@ -1,13 +1,16 @@
 #include <arpa/inet.h>
 #include <ctype.h>
 #include <dirent.h>
+#include <errno.h>
 #include <fcntl.h>
 #include <netinet/in.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/select.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 // creating struct to store the information about the processes in the server
